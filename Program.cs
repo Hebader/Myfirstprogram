8f95ea53e36dbd57ed6cd7002030a6931948a1fc
Heba Derawi <heba.derawi95@hotmail.com>
2023-09-12 11:41:31 +0200

    ändring

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleApp3
+﻿//Heba Derwi, NET23
+
+int nummer = 15; //Ger ett värde till variabeln
+
+if (nummer > 10) // Om värdet är över 10 
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-        
-        }
-    }
+    Console.WriteLine("Talet är stort!"); //utskrift
+}
+else
+{
+    Console.WriteLine("Oj lågt tal!"); // Om värdet ej är över 10
+}
+
+Console.WriteLine("Vad heter du?");
+var namn = Console.ReadLine(); // Användaren skriver in namn
+Console.WriteLine("Hej" + namn + "!");
+
+for (int i = 0; i <= nummer; i = i + 1) // En for loop som skriver ut 0-15.
+{
+    Console.WriteLine(i);
 }
